0fcb32dcb1dc0251d07a9d5a1c1d9e40381defb3
Yaniv Ben Hamo <yanivben3@gmail.com>
2019-03-08 18:38:49 +0200

    - insert new docs function - Find doc meta data

--- files_utils.py
+++ files_utils.py
@@ -7,20 +7,52 @@ import nltk
 
 # ----- Reading files from awaiting_documents folder -----
 path = 'awaiting_documents/*.txt'
-awaiting_docs_path = glob.glob(path)
+docs_path = glob.glob(path)
 
 # ----- Pull Document from 'awaiting_documents' and start handle him -----
 def Pull_Documents():
     docs_arr = []
     # Read each doc that located in the wait docs folder
     # and put them inside 'docs_arr' array
-    for doc_name in awaiting_docs_path:
+    for doc_name in docs_path:
         try:
             docs_arr.append(open(doc_name))
         except IOError as exc:
             if exc.errno != errno.EISDIR:
                 raise
     return docs_arr
+
+def Get_Doc_MetaData(doc_index):
+    # Find occurrence of the word 'by'
+    # For doc name
+    index = 0
+    doc_name=""
+    for word in doc_index:
+        if(word == 'by'):
+            break
+        index = index + 1
+    for word in range(index):
+        doc_name += doc_index[word] + " "
+    # remove the space after the last word
+    doc_name = doc_name.rstrip()
+    # END doc name
+
+    # Find occurrence of the char ':'
+    # For doc author
+    index = 0
+    doc_name = ""
+    for word in doc_index:
+        if (word == ':'):
+
+            break
+        index = index + 1
+    for word in range(index):
+        doc_name += doc_index[word] + " "
+    # remove the space after the last word
+    doc_name = doc_name.rstrip()
+    # END doc author
+
+    return doc_name
 
 
     # ----- Creating index files -----
@@ -30,8 +62,12 @@ def Create_Doc_Index(doc):
     return nltk_tokens
 
 
-# ----- Moving files from awaiting_documents folder to Documents folder -----
-def moveDocBetweenDirs(doc):
+# ----- Moving files from awaiting_documents folder to documents folder -----
+def moveDocsBetweenDirs():
     source = 'awaiting_documents/'
-    dest = 'Documents/'
-    shutil.move(source+doc, dest)
+    dest = 'documents/'
+    files = os.listdir(source)
+
+    for f in files:
+        shutil.move(source+f, dest+f)
+    return True
--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 from nltk.corpus import stopwords
 import parse_file
 import files_utils
+import time
 
-docs_arr = []
 
-if __name__ == '__main__':
-    docs_arr = files_utils.Pull_Document()  # Retrieve the docs available in the wait folder
+def Insert_New_Docs():
+    docs_arr = files_utils.Pull_Documents()  # Retrieve the docs available in the wait folder
     for doc in docs_arr:
-        files_utils.Create_Doc_Index(doc)
+        doc_index = files_utils.Create_Doc_Index(doc)
+        print(doc_index)
+        doc_name = files_utils.Get_Doc_MetaData(doc_index)
         DocCol = parse_file.connectToDB()
         doc_id = parse_file.findMaxDocID(DocCol)  # new id for the new doc
-    #print(stopwords.words('english'))
+        print(str(int(doc_id)) + " " + doc_name)
+    # files_utils.moveDocsBetweenDirs()
+    # print(stopwords.words('english'))
+
+
+def log(msg):
+    print("- " + msg + "\n")
+
+
+if __name__ == '__main__':
+    Insert_New_Docs()
     exit(0)
