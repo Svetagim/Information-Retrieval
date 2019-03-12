cb2a6143f2654c38df114b0c9120dbed7895d920
Yaniv Ben Hamo <yaniv.bhemo@gmail.com>
2019-03-12 13:15:04 +0200

    Parsing table into db

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from nltk.corpus import stopwords
 import parse_file
 import files_utils
-import re
 
 
 def Insert_New_Docs():
@@ -9,23 +8,14 @@ def Insert_New_Docs():
     for doc in docs_arr:
         doc_index = parse_file.Create_Doc_Index(doc)
         doc_metadata = files_utils.Get_Doc_MetaData(doc_index)
-        docsCollection = parse_file.connectToDB()
+        docsCollection = parse_file.connectToDB("docCollection")
         doc_id = parse_file.findMaxDocID(docsCollection)  # new id for the new doc
         doc_metadata.append(doc_id)
         log("Processing: " + str(int(doc_id)) + " " + doc_metadata[0] + " " + doc_metadata[1] + " " + doc_metadata[2])
         parse_file.Insert_New_Doc_Record_to_DB(doc_metadata, docsCollection)
         log("The new doc has documented in the DB")
-
-        #Building the inverted table
-        # - Filtering
-        index_to_delete=0
-        for word in doc_index:
-            word=word.lstrip()
-            word=word.rstrip()
-            if ( str(re.findall(r'[a-zA-Z]+', word)) == "[]" ):
-                del doc_index[index_to_delete]
-                print(doc_index)
-            index_to_delete += 1
+        log("Build inverted file")
+        parse_file.Build_Invert_File(doc_index, doc_id)
 
     # files_utils.moveDocsBetweenDirs()
     # print(stopwords.words('english'))
--- parse_file.py
+++ parse_file.py
@@ -1,12 +1,13 @@
 import pymongo
 import nltk
+import re
 
 
-def connectToDB():
+def connectToDB(collection):
     myClient = pymongo.MongoClient("mongodb://127.0.0.1:27017/")
     mydb = myClient["ir"]
-    DocCol = mydb["docCollection"]
-    return DocCol
+    col = mydb[collection]
+    return col
 
 
 def findMaxDocID(DocCol):
@@ -26,6 +27,7 @@ def Create_Doc_Index(doc):
     nltk_tokens = nltk.word_tokenize(sentence_data)
     return nltk_tokens
 
+
 def Insert_New_Doc_Record_to_DB(doc_metadata, docCollection):
     myquery = {
         "id": doc_metadata[3],
@@ -34,3 +36,22 @@ def Insert_New_Doc_Record_to_DB(doc_metadata, docCollection):
         "date_of_create": doc_metadata[2]
     }
     docCollection.insert_one(myquery)
+
+
+def Build_Invert_File(doc_index, doc_id):
+    # Building the inverted table
+    # - Filtering + Parsing
+    invertColl = connectToDB("invertFile")
+    index_to_delete = 0
+    for word in doc_index:
+        word = word.lstrip()
+        word = word.rstrip()
+        if (str(re.findall(r'[a-zA-Z]+', word)) == "[]"):
+            del doc_index[index_to_delete]
+        else:
+            myquery = {
+                "Term": word,
+                "Doc Id": doc_id
+            }
+            invertColl.insert_one(myquery)
+        index_to_delete += 1
